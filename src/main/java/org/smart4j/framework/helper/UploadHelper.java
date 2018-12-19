65a41a600ca553191746807278f11fd2e0169473
autumn <qy814379857@foxmail.com>
2018-12-19 13:32:20 +0800

    文件上传操作类

--- src/main/java/org/smart4j/framework/helper/UploadHelper.java
+++ src/main/java/org/smart4j/framework/helper/UploadHelper.java
@@ -1,13 +1,28 @@
 package org.smart4j.framework.helper;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.smart4j.framework.bean.FileParam;
+import org.smart4j.framework.bean.FormParam;
+import org.smart4j.framework.bean.Param;
+import org.smart4j.framework.util.CollectionUtil;
+import org.smart4j.framework.util.FileUtil;
+import org.smart4j.framework.util.StringUtil;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @program: UploadHelper
@@ -42,4 +57,47 @@ public final class UploadHelper {
         return ServletFileUpload.isMultipartContent(request);
     }
 
+    /**
+     * 创建请求对象
+     * @return
+     */
+    public static Param createParam(HttpServletRequest request){
+        List<FormParam> formParamList = new ArrayList<FormParam>();
+        List<FileParam> fileParamList = new ArrayList<FileParam>();
+
+        try{
+            Map<String,List<FileItem>> fileItemListMap = servletFileUpload.parseParameterMap(request);   //将request转换为Map
+            if (CollectionUtil.isNotEmpty(fileItemListMap)){
+                for (Map.Entry<String,List<FileItem>> fileItemListEntry : fileItemListMap.entrySet()){
+                    String fieldName = fileItemListEntry.getKey();    //获取表单字段名
+                    List<FileItem> fileItemList = fileItemListEntry.getValue();
+
+                    if (CollectionUtil.isNotEmpty(fileItemListMap)){
+                        for (FileItem fileItem:fileItemList){
+                            if (fileItem.isFormField()){   //如果是表单字段
+                                String fieldValue = fileItem.getString("UTF-8");
+                                formParamList.add(new FormParam(fieldName,fieldValue));
+                            }else{   //如果是文件
+                                String fileName = FileUtil.getRealFileName(new String(fileItem.getName().getBytes(),"UTF-8"));   //获取文件名
+                                if (StringUtil.isNotEmpty(fileName)){  //如果文件名不为空
+                                    long fileSize = fileItem.getSize();  //获取文件大小
+                                    String contentType = fileItem.getContentType();   //获取文件类型
+                                    InputStream inputStream = fileItem.getInputStream();   //获取文件输入流
+                                    fileParamList.add(new FileParam(fieldName,fileName,fileSize,contentType,inputStream));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (FileUploadException e) {
+            LOGGER.error("create param failure",e);
+            throw new RuntimeException(e);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Param(formParamList,fileParamList);
+    }
 }
